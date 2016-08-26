d34335f1a2da1c5781a6ef0531a20b4fb903628d
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-08-26 14:55:41 +0200

    resolved more challenges

---
+++ CPP/DequeSTL.cpp
@@ -0,0 +1,90 @@
+//
+//  DequeSTL.cpp
+//  HackerRank
+//
+//  Created by Duroni Fabrizio on 25/08/16.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+//  https://www.hackerrank.com/challenges/deque-stl
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ I must admit I was not able to solve this with dequeu.
+ This is the first challenge in C++ section that I was not able to solve.
+ I get "inspired" from this link http://www.geeksforgeeks.org/maximum-of-all-subarrays-of-size-k/#disqus_thread"
+ reported in the discussion tab.
+ So I didn't solve this challenge, but I learned something new :) (and found geeksforgeeks.com, a wonderful site XD).
+ */
+void printKMax(int arr[], int n, int k) {
+    
+    deque<int> buffer;
+    
+    for (int i = 0; i < k; ++i) {
+        
+        // For very element, the previous smaller elements are useless so remove them from Qi.
+        while((!buffer.empty()) && arr[i] >= arr[buffer.back()]) {
+            
+            buffer.pop_back();
+        }
+        
+        // Add new element at rear of queue.
+        buffer.push_back(i);
+    }
+    
+    // Process rest of the elements, i.e., from arr[k] to arr[n-1]
+    for (int i = k; i < n; ++i) {
+        
+        // The element at the front of the queue is the largest element of
+        // previous window, so print it.
+        cout << arr[buffer.front()] << " ";
+        
+        // Remove the elements which are out of this window.
+        while ((!buffer.empty()) && buffer.front() <= i - k) {
+            
+            buffer.pop_front();
+        }
+        
+        while ((!buffer.empty()) && arr[i] >= arr[buffer.back()]) {
+
+            buffer.pop_back();
+        }
+        
+        // Add current element at the rear of Qi
+        buffer.push_back(i);
+    }
+    
+    cout << arr[buffer.front()] << " " << endl;
+}
+
+int main() {
+    
+    int t;
+    
+    cin >> t;
+    
+    while(t>0) {
+        
+        int n,k;
+        
+        cin >> n >> k;
+        
+        int i;
+        int arr[n];
+        
+        for(i=0;i<n;i++) {
+            
+            cin >> arr[i];
+        }
+        
+        printKMax(arr, n, k);
+        
+        t--;
+    }
+    
+    return 0;
+}
---
+++ CPP/MagicSpells.cpp
@@ -0,0 +1,140 @@
+//
+//  MagicSpells.cpp
+//  HackerRank
+//
+//  Created by Duroni Fabrizio on 26/08/16.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+//  https://www.hackerrank.com/challenges/magic-spells
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Spell {
+private:
+    string scrollName;
+public:
+    Spell(): scrollName("") { }
+    Spell(string name): scrollName(name) { }
+    virtual ~Spell() { }
+    string revealScrollName() {
+        return scrollName;
+    }
+};
+
+class Fireball : public Spell {
+private: int power;
+public:
+    Fireball(int power): power(power) { }
+    void revealFirepower(){
+        cout << "Fireball: " << power << endl;
+    }
+};
+
+class Frostbite : public Spell {
+private: int power;
+public:
+    Frostbite(int power): power(power) { }
+    void revealFrostpower(){
+        cout << "Frostbite: " << power << endl;
+    }
+};
+
+class Thunderstorm : public Spell {
+private: int power;
+public:
+    Thunderstorm(int power): power(power) { }
+    void revealThunderpower(){
+        cout << "Thunderstorm: " << power << endl;
+    }
+};
+
+class Waterbolt : public Spell {
+private: int power;
+public:
+    Waterbolt(int power): power(power) { }
+    void revealWaterpower(){
+        cout << "Waterbolt: " << power << endl;
+    }
+};
+
+class SpellJournal {
+public:
+    static string journal;
+    static string read() {
+        return journal;
+    }
+};
+string SpellJournal::journal = "";
+
+void counterspell(Spell *spell) {
+
+    if(Fireball* fireBall = dynamic_cast<Fireball*>(spell)) {
+        
+        fireBall->revealFirepower();
+    } else if(Frostbite* frostBite = dynamic_cast<Frostbite*>(spell)) {
+        
+        frostBite->revealFrostpower();
+    } else if(Thunderstorm* thunderstorm = dynamic_cast<Thunderstorm*>(spell)) {
+        
+        thunderstorm->revealThunderpower();
+    } else if(Waterbolt* waterbolt = dynamic_cast<Waterbolt*>(spell)) {
+
+        waterbolt->revealWaterpower();
+    } else {
+        
+        string spellRevealed = spell->revealScrollName();
+        string spellRead = SpellJournal::read();
+        
+        int countSameLetters = 0;
+        
+        for (int i = 0; i < spellRead.length(); i++) {
+            
+            if(spellRevealed.find_first_of(spellRead[i]) != string::npos) {
+                
+                countSameLetters++;
+            }
+        }
+        
+        cout << countSameLetters << endl;
+    }
+}
+
+class Wizard {
+public:
+    Spell *cast() {
+        Spell *spell;
+        string s; cin >> s;
+        int power; cin >> power;
+        if(s == "fire") {
+            spell = new Fireball(power);
+        }
+        else if(s == "frost") {
+            spell = new Frostbite(power);
+        }
+        else if(s == "water") {
+            spell = new Waterbolt(power);
+        }
+        else if(s == "thunder") {
+            spell = new Thunderstorm(power);
+        }
+        else {
+            spell = new Spell(s);
+            cin >> SpellJournal::journal;
+        }
+        return spell;
+    }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    Wizard Arawn;
+    while(T--) {
+        Spell *spell = Arawn.cast();
+        counterspell(spell);
+    }
+    return 0;
+}
+
