5cdbccf192cd3ebe8e19ae263a3e82918b6a9d5e
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-09-05 21:47:10 +0200

    resolved more challenges

---
+++ Algorithms/InsertionSortPart2.cpp
@@ -0,0 +1,69 @@
+//
+//  InsertionSortPart2.cpp
+//  HackerRank
+//
+//  Created by Fabrizio Duroni on 05/09/16.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+//  https://www.hackerrank.com/challenges/insertionsort2
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void printVector(vector<int> ar) {
+    
+    for (int i = 0; i < ar.size(); i++) {
+        
+        cout << ar[i] << " ";
+    }
+    
+    cout << endl;
+}
+
+void insertionSort(vector<int> ar) {
+    
+    int value;
+    int j;
+    int temp;
+    
+    for (int i = 1; i < ar.size(); i++) {
+        
+        value = ar[i];
+        j = i - 1;
+        
+        while (j >= 0 && ar[j] > value) {
+            
+            temp = ar[j + 1];
+            ar[j + 1] = ar[j];
+            ar[j] = temp;
+            
+            j--;
+        }
+        
+        ar[j + 1] = value;
+        
+        printVector(ar);
+    }
+}
+
+int main(void) {
+    
+    vector <int>  _ar;
+    
+    int _ar_size;
+    
+    cin >> _ar_size;
+    
+    for(int _ar_i=0; _ar_i<_ar_size; _ar_i++) {
+        
+        int _ar_tmp;
+        cin >> _ar_tmp;
+        _ar.push_back(_ar_tmp);
+    }
+    
+    insertionSort(_ar);
+    
+    return 0;
+}
