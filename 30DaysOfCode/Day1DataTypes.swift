48c79b31dd1074f7a0fc0ef65472007b2b24b800
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-09-18 15:49:15 +0200

    more challenges resolved

---
+++ 30DaysOfCode/Day1DataTypes.swift
@@ -0,0 +1,34 @@
+//
+//  Day1DataTypes.swift
+//  HackerRank
+//
+//  Created by Fabrizio Duroni on 18/09/16.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+//  https://www.hackerrank.com/challenges/30-data-types
+
+import Foundation
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var i2:Int
+var d2:Double
+var s2:String
+
+// Read and save an integer, double, and String to your variables.
+i2 = Int(readLine()!)!
+d2 = Double(readLine()!)!
+s2 = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i + i2)
+
+// Print the sum of the double variables on a new line.
+print(d + d2)
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s + s2)
