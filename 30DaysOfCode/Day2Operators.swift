3482154cb3f5975d6378a552a52db1cceb5d6790
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-09-19 14:35:06 +0200

    more challenges resolved

---
+++ 30DaysOfCode/Day2Operators.swift
@@ -0,0 +1,20 @@
+//
+//  Day2Operators.swift
+//  HackerRank
+//
+//  Created by Duroni Fabrizio on 19/09/16.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+//  https://www.hackerrank.com/challenges/30-operators
+
+import Foundation
+
+let mealCost:Float = Float(readLine()!)!
+let tipPercent:Int = Int(readLine()!)!
+let taxPercent:Int = Int(readLine()!)!
+
+let tip:Float = mealCost * (Float(tipPercent)/100.0)
+let tax:Float = mealCost * (Float(taxPercent)/100.0)
+var totalCost = round(mealCost + tip + tax)
+
+print("The total meal cost is \(Int(totalCost)) dollars.")
