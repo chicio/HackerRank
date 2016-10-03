685a9b70358f8988e16a8b99733cbdd452f798df
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-10-03 14:04:30 +0200

    more challenged resolved

---
+++ 30DaysOfCode/Day16ExceptionsStringtoInteger.swift
@@ -0,0 +1,44 @@
+//
+//  Day16ExceptionsStringtoInteger.swift
+//  HackerRank
+//
+//  Created by Duroni Fabrizio on 03/10/2016.
+//
+//  https://www.hackerrank.com/challenges/30-exceptions-string-to-integer
+
+import Foundation
+
+/*
+ * Define an ErrorType
+ */
+enum StringToIntTypecastingError: Error {
+    case BadString
+}
+
+/*
+ * If typecasting is not possible, throw exception, otherwise return the Integer value
+ */
+func stringToInt(inputString: String) throws -> Int {
+    
+    let integerValue = Int(inputString)
+    
+    guard integerValue != nil else {
+        
+        throw StringToIntTypecastingError.BadString
+    }
+    
+    return integerValue!
+}
+
+/*
+ * Read the input
+ */
+let inputString = readLine()!
+
+do {
+    
+    print(try stringToInt(inputString: inputString))
+} catch StringToIntTypecastingError.BadString {
+    
+    print("Bad String")
+}
