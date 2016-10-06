431ca804bad295ffe97f1ee5bdf9302d95157f3e
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-10-06 02:13:46 +0200

    more challenges resolved

---
+++ 30DaysOfCode/Day18QueuesandStacks.swift
@@ -0,0 +1,70 @@
+//
+//  Day18QueuesandStacks.swift
+//  HackerRank
+//
+//  Created by Fabrizio Duroni on 06/10/2016.
+//  Copyright © 2016 Fabrizio Duroni. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    //Write your code here
+    
+    var stack = [Character]()
+    var queue = [Character]()
+
+    func pushCharacter(ch: Character) -> Void {
+    
+        stack.append(ch)
+    }
+ 
+    func popCharacter() -> Character {
+
+        return stack.removeLast()
+    }
+
+    func enqueueCharacter(ch: Character) -> Void {
+        
+        queue.append(ch)
+    }
+    
+    func dequeueCharacter() -> Character {
+        
+        return queue.removeFirst()
+    }
+}
+
+// read the string s.
+let s = readLine()!
+
+// create the Solution class object p.
+let obj = Solution()
+
+// push/enqueue all the characters of string s to stack.
+for c in s.characters {
+    
+    obj.pushCharacter(ch: c)
+    obj.enqueueCharacter(ch: c)
+}
+
+var isPalindrome = true
+
+// pop the top character from stack.
+// dequeue the first character from queue.
+// compare both the characters.
+for i in 0 ..< (s.characters.count / 2) {
+    
+    if obj.popCharacter() != obj.dequeueCharacter() {
+        
+        isPalindrome = false
+        break
+    }
+}
+
+// finally print whether string s is palindrome or not.
+if isPalindrome {
+    print("The word, \(s), is a palindrome.")
+} else {
+    print("The word, \(s), is not a palindrome.")
+}
