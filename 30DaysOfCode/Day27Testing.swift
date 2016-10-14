4906a31d4c791997b519b05dd9ac105372ccbd87
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-10-14 13:38:57 +0200

    more challenges resolved

---
+++ 30DaysOfCode/Day27Testing.swift
@@ -0,0 +1,19 @@
+//
+//  Day27Testing.swift
+//  HackerRank
+//
+//  Created by Duroni Fabrizio on 14/10/2016.
+//  
+//  https://www.hackerrank.com/challenges/30-testing
+
+import Foundation
+
+//As stupid as nothing else....
+print("3")
+print("4 3")
+print("-1 -3 0 2")
+print("5 2")
+print("0 -1 2 1 4")
+print("3 1")
+print("-1 0 1")
+
