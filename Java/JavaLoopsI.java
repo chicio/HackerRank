90898f3451e8f75c02f936418e170a63572382b1
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2016-11-17 02:42:54 +0100

    more challenges resolved

---
+++ Java/JavaLoopsI.java
@@ -0,0 +1,24 @@
+//
+//  JavaLoopsI.java
+//  HackerRank
+//
+//  Created by Fabrizio Duroni on 17/11/2016.
+//
+//  https://www.hackerrank.com/challenges/java-loops-i
+
+import java.util.Scanner;
+
+public class JavaLoopsI {
+
+    public static void main(String[] args) {
+    	
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        
+        for (int i = 1; i <= 10; i++) {
+        	
+        	System.out.printf("%d x %d = %d \n", n, i, n * i);
+        }
+    }
+}
