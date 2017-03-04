33ffb08b19e214ebd3a21b0a960a303c7aad5406
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2017-03-04 14:42:22 +0100

    more challenges resolved

--- Java/it/hackerrank/datastructures/Java1DArrayPart2.java
+++ Java/it/hackerrank/datastructures/Java1DArrayPart2.java
@@ -1,8 +1,36 @@
+//
+//  PrimeChecker.java
+//  HackerRank
+//
+//  Created by Fabrizio Duroni on 04/03/2017.
+//
+//  https://www.hackerrank.com/challenges/java-1d-array
+
 package it.hackerrank.datastructures;
 
 import java.util.Scanner;
 
+/// Neeeded a little help with discussion.
 public class Java1DArrayPart2 {
+	
+	public static boolean couldWin(int[] array, int currentPosition, int maxJump) {
+		
+		if (currentPosition >= array.length) {
+			
+			return true;
+		}
+		
+		if (currentPosition < 0 || array[currentPosition] == 1) {
+			
+			return false;
+		}
+		
+		array[currentPosition] = 1;
+		
+		return couldWin(array, currentPosition - 1, maxJump) || 
+				couldWin(array, currentPosition + 1, maxJump) ||
+				couldWin(array, currentPosition + maxJump, maxJump);
+	}
 
 	public static void main(String[] args) {
 
@@ -17,45 +45,13 @@ public class Java1DArrayPart2 {
 			n = sc.nextInt();
 			m = sc.nextInt();
 			array = new int[n];
-			boolean[] explored = new boolean[n];
 			
 			for (int h = 0; h < n; h++) {
 				
 				array[h] = sc.nextInt();
-				explored[h] = false;
 			}
 			
-			boolean gameFinished = false;
-			boolean gameWin = false;
-			int position = 0;
-			
-			while (!gameFinished) {
-				
-				if (position + m > array.length - 1 || position + 1 > array.length - 1) {
-					
-					gameFinished = true;
-					gameWin = true;
-				} else {
-					
-					explored[position] = true;
-					
-					if (!explored[position + m] && array[position + m] == 0) {
-						
-						position = position + m;
-					} else if(!explored[position + 1] && array[position + 1] == 0) {
-						
-						position = position + 1;
-					} else if(position - 1 >= 0 && !explored[position - 1] && array[position - 1] == 0) {
-						
-						position = position - 1;
-					} else {
-						
-						gameFinished = true;
-					}					
-				}
-			}
-			
-			if (gameWin) {
+			if (couldWin(array, 0, m)) {
 				
 				System.out.println("YES");
 			} else {
