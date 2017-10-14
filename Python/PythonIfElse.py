d0c6b9529cddbd9070c7d0a477972419334e4c2a
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2017-10-14 14:19:12 +0200

    More challenges resolved

---
+++ Python/PythonIfElse.py
@@ -0,0 +1,11 @@
+n = int(raw_input())
+
+if n % 2 != 0:
+    print "Weird"
+else:
+    if 2 <= n <= 5:
+        print "Not Weird"
+    if 6 <= n <= 20:
+        print "Weird"
+    if n > 20:
+        print "Not Weird"
